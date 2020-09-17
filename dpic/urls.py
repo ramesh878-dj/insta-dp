92c0027219784c486b89da004d59b35db24665b7
ramesh878-dj <rameshsolaimalai878@gmail.com>
2020-09-17 20:56:41 +0530

    urls

--- dpic/urls.py
+++ dpic/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('api/', include('instapic.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
